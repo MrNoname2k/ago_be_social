a6ae8b7f6fea20a24e867957d769301a4832c303
LinhNguyen <nxlinh.12b10.pdl@gmail.com>
2023-07-09 14:39:01 +0700

    update: message feature

--- src/main/java/org/api/payload/response/homePageResponses/HomePageResponse.java
+++ src/main/java/org/api/payload/response/homePageResponses/HomePageResponse.java
@@ -22,7 +22,7 @@ public class HomePageResponse implements Serializable {
     private UserHomeRespon userEntity;
     private List<RelationshipResponse> relationshipEntities;
     private PostHomePageResponse postEntityPage;
-    private MessagePageResponse messageEntityPage;
+//    private MessagePageResponse messageEntityPage;
     private NotifiHomePageResponse notificationEntityPage;
 //    private List<PostHomeRespon> postAvatar;
 //    private List<PostHomeRespon> postBanner;
